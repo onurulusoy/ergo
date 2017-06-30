af1821ebcdbe16c21f22d41afe632ddef6d5d9df
onurulusoy <ulusoyonur@yahoo.com>
2017-06-30 22:01:09 +0300

    version1.0

--- basic_test.js
+++ basic_test.js
@@ -1,97 +1,56 @@
 Feature('CodeceptJS demo');
 
-//Scenario('check Welcome page on site', (I) => {
-//I.amOnPage('/');
-//I.see('Ucuz Uçak Bileti');
-//I.see('PEGASUS');
-//     //I.fillField('q', 'hasan kaya');
-//    // I.pressKey("Enter"); //will be deprecated soon
-//  //   I.wait(3);
-////     I.see("Hasan Kaya Profiles | Facebook");
-////     I.click("Hasan Kaya Profiles | Facebook");
-//  I.saveScreenshot("mainPage.jpg");
-//});
-
-Scenario('search cheap flight', (I) => {
+Scenario('Ergo Sigorta Main Page', (I) => {
     I.amOnPage('/');
-    I.see('Ucuz Uçak Bileti Ara');
-    I.click('span.select2-selection__placeholder');
-    I.fillField('/html/body/span/span/span[1]/input', 'SAW');
-    //I.moveCursorTo('/html/body/span/span/span[2]/ul/li');
-    I.click('/html/body/span/span/span[2]/ul/li');
-    I.fillField('/html/body/span/span/span[1]/input', 'HTY');
-    I.scrollTo('/html/body/div[2]/section[2]/div/div/div[2]/div[1]/form/div[3]/div[1]/span[1]/div/div[2]/div/div[1]/span[1]');
-    I.click('/html/body/span/span/span[2]/ul/li');
-    I.wait(2);
-    I.click('/html/body/div[2]/section[2]/div/div/div[2]/div[1]/form/div[3]/div[1]/span[1]/div/div[2]/div/div[2]/div/div[2]/table/tbody/tr[3]/td[1]');
-    I.click('/html/body/div[2]/section[2]/div/div/div[2]/div[1]/form/div[3]/div[2]/span[1]/div/div[2]/div/div[2]/div/div[1]/table/tbody/tr[4]/td[1]');
-    I.click('UCUZ UÇUŞ ARA');
-    I.wait(7);
+    I.seeInTitle('ERGO Sigorta');
+    I.see('En Yakın ERGO Acentesi');
+    I.see('Anlaşmalı Servisler');
+    I.see('Anlaşmalı Sağlık Kuruluşları');
+    I.see('Hasar Durumu Hakkında Bilgi Alın');
     I.resizeWindow('maximize');
-    I.wait(3);
-    //     I.fillField('q', 'hasan kaya');
-    //     I.pressKey("Enter"); //will be deprecated soon
-    //     I.wait(3);
-    //     I.see("Hasan Kaya Profiles | Facebook");
-    //     I.click("Hasan Kaya Profiles | Facebook");
-    I.saveScreenshot("mainPage.jpg");
+    I.saveScreenshot("ergoMainPage.jpg");
+
 });
 
-// Scenario('check another test', (I) => {
-//     I.amOnPage('http://www.facebook.com');
-//     I.see('Email or Phone');
-//     I.see('Facebook');
-//     I.fillField('email', 'onrulusoy');
-//     I.fillField('pass', 'asdf');
-//     I.click('Log In');
-//     I.wait(1);
-//     I.seeElement('//*[@class="_4rbf _53ij"]');
-// })
+Scenario('En Yakın ERGO Acentesi', (I) => {
+    I.amOnPage('/');
+    I.scrollTo('//*[@id="enYakinErgoCity"]');
+    I.selectOption('enYakinErgoCity', 'İSTANBUL');
+    I.waitForText('PENDİK', 2);
+    I.selectOption('enYakinErgoDistrict', 'PENDİK');
+    I.click('//*[@id="enYakinErgoHomePageForm"]/button');
+    I.waitForText('2K SİGORTA ARACILIK HİZMETLERİ LTD.ŞTİ.', 5);
+    I.saveScreenshot('acente.jpg')
 
-// Scenario('Google Image Search', (I) => {
-//     I.amOnPage('/');
-//     I.see('Gmail');
-//     I.fillField('q', 'volkan kumbasar');
-//     I.pressKey("Enter"); //will be deprecated soon
-//     I.wait(3);
-//     I.click('//*[@id="hdtb-msb-vis"]/div[2]/a');
-//     I.wait(2);
-//     I.saveScreenshot("kumbasar.jpg");
-// })
+});
 
-//Scenario('Google Image Search', (I) => {
-//I.amOnPage('http://www.aksigorta.com');
-//I.scrollTo('//*[@class="jquery-selectbox-moreButton"]');
-//I.click('//*[@class="jquery-selectbox-moreButton"]');
-//    I.see('Ailemi Kapsasın');
-//    I.see('Evimi Kapsasın');
-//    I.see('Aracımı Kapsasın');
-//    I.see('İşimi Kapsasın');
-//I.saveScreenshot("combobox.jpg");
-//});
+Scenario('Anlaşmalı Servisler', (I) => {
+    I.amOnPage('/');
+    I.scrollTo('//*[@id="anlasmaliServislerCity"]');
+    I.selectOption('anlasmaliServislerCity', 'ANKARA');
+    I.waitForText('GÖLBAŞI', 2);
+    I.selectOption('anlasmaliServislerDistrict', 'GÖLBAŞI');
+    I.click('//*[@id="anlasmaliServislerHomepageForm"]/button');
+    I.waitForElement('//*[@id="anlasmaliServislerMarka"]', 2);
+    I.seeCurrentUrlEquals('/ergo-anlasmali-oto-servisler');
+    I.waitForText('ANKARA', 5, '//*[@selected="selected"]');
+    I.waitForText('GÖLBAŞI', 5, '//*[@selected="selected"]');
+    I.selectOption('anlasmaliServislerMarka', 'BMW');
+    I.selectOption('anlasmaliServislerOnarimYeri', 'Tüm Servisler');
+    I.click('Ara');
+    I.waitForText('Seçtiğiniz kriterlere uygun sonuç bulunamadı.', 5);
+    I.saveScreenshot('anlasmaliServisler.jpg')
 
-//Scenario('Custom Func Test', (I) => {
-//    I.amOnPage("http://etkinlikcim-new-design.eu-west-1.elasticbeanstalk.com");
-//    I.checkImage();
-// I.logYaz("ne yaptın");
-//});
+});
 
-// Scenario('Custom Func Test', (I) => {
-//     I.tryThat();
-// })
+Scenario('Kurumsal Ürünler', function*(I) {
+    I.amOnPage('/');
+    I.click("//span[text()='Kurumsal Ürünler']");
+    I.waitForVisible('//*[@class="icon-atom icon-m icon"]', 5);
+    I.click('//*[@id="Header"]/div/div[3]/ul[1]/li[2]/div/div/ul[3]/li/h3/a');
+    I.seeCurrentUrlEquals("muhendislik-sigortalari");
+    let link = yield I.grabAttributeFrom('//*[@class="content"]/img', 'src');
+    I.checkImage(link);
+    I.saveScreenshot('mühendislikSigortaları.jpg');
 
-//Scenario('Custom Func Test', (I) => {
-//I.loginAS01();
-//I.see('Provisioning Client');
-//   I.asdf();
-//I.see('Welcome to the Provisioning Client');
-//I.see('Logged in as: admin');
-//I.see("Services");
-//I.click('//*[@id="jbpns_2fprov_2fdefault_2fProvMainMenuWindowsnpbj:j_id_jsp_891425604_0:j_id_jsp_891425604_6:j_id_jsp_891425604_7"]/span'); //services
-//    I.selectOption('Services', 'Resources');
-//    I.wait(1);
-//    I.click('//*[@id="refjbpns_2fprov_2fdefault_2fProvMainMenuWindowsnpbj:j_id_jsp_891425604_0:j_id_jsp_891425604_6:j_id32"]'); //announcements
-//    I.wait(1);
-//    I.click('//*[@id="jbpns_2fprov_2fdefault_2fProvMainMenuWindowsnpbj:j_id_jsp_891425604_0:j_id_jsp_891425604_6:j_id49"]'); //branding
-//    I.wait(2);
-//});
+});
--- steps_file.js
+++ steps_file.js
@@ -3,42 +3,13 @@
 var https = require('https');
 var request = require('sync-request');
 const assert = require('assert');
-//var $ = require("jquery");
-
-
 
 module.exports = function() {
     return actor({
 
-        checkImage() {
-            var res = request('GET', 'http://res.cloudinary.com/etkinlikcim/image/upload/c_crop,h_810,w_545,g_center/l_watermark-etkinlikcim,y_100,w_0.5,fl_relative,g_south/v1475074824/live/vendorImages/soho-house/cover/1475074823136.jpg');
-            assert.strictEqual(res.statusCode, 200);
-        },
-
-        tryThat() {
-            this.amOnPage("http://www.github.com");
-        },
-
-        loginAS01() {
-            this.amOnPage('https://47.168.105.72:8443/portal/j_security_check');
-            this.fillField('j_username', 'admin');
-            this.fillField('j_password', 'admin');
-            this.click('login');
-        },
-
-        getClassNameFromText() {
-            var className = this.document.$("span:contains('Services')").attr("class");
-
-            console.log(className);
-        },
-
-        asdf() {
-            var className = $("span:contains('Services')").getAttribute();
-            this.click(selector);
-        },
-
-        test() {
-            this._locateClickable('Services');
+        checkImage(link) {
+            var response = request('GET', link);
+            assert.strictEqual(response.statusCode, 200);
         }
 
         // Define custom steps here, use 'this' to access default methods of I.
